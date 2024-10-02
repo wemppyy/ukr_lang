e3ec4003ea30eb567a89592625b1cb4bac112858
wemppyy <denisfrostaliuk@gmail.com>
2024-10-02 18:59:40 +0300

    Add project files.

---
+++ ukr_lang.h
@@ -0,0 +1,150 @@
+#ifndef UKR_LANG
+#define UKR_LANG
+
+// Configs
+#define LANG_NAME "Українська"
+#define використати using
+#define простір_імен namespace
+#define стандартний std
+#define вивести cout
+#define кінець_рядка endl
+#define головна int main()
+#define повернути return
+#define нуль 0
+#define ввести cin
+#define якщо if
+#define інакше else
+#define для for
+#define поки while
+#define робити do
+#define допоки while
+#define вибрати switch
+#define випадок case
+#define вийти break
+#define продовжити continue
+#define встановити set
+#define отримати get
+#define вказівник pointer
+#define масив array
+#define структура struct
+#define клас class
+#define публічний public
+#define приватний private
+#define захищений protected
+#define конструктор constructor
+#define деструктор destructor
+#define віртуальний virtual
+#define перевизначення override
+#define перевантаження overload
+#define віртуальний_клас abstract
+#define новий new
+#define видалити delete
+#define розмір size
+#define вивести_розмір sizeof
+#define вивести_память malloc
+#define звільнити_память free
+#define вивести_память_заповнити calloc
+#define звільнити_память_заповнити free
+#define змінити_розмір_памяті realloc
+#define замовчування default
+
+// Data types
+#define рядок string
+#define символ char
+#define ціле int
+#define довге long
+#define коротке short
+#define плаваюче float
+#define подвійне double
+#define беззнакове unsigned
+#define вказівник_на_ціле int*
+#define вказівник_на_довге long*
+#define вказівник_на_коротке short*
+#define вказівник_на_плаваюче float*
+#define вказівник_на_подвійне double*
+#define вказівник_на_беззнакове unsigned*
+#define вказівник_на_символ char*
+#define вказівник_на_рядок string*
+#define вказівник *
+#define вказівник_на_вказівник **
+#define вказівник_на_масив array*
+#define вказівник_на_структура struct*
+#define вказівник_на_клас class*
+#define вказівник_на_віртуальний virtual*
+#define вказівник_на_публічний public*
+#define вказівник_на_приватний private*
+#define вказівник_на_захищений protected*
+#define вказівник_на_конструктор constructor*
+#define вказівник_на_деструктор destructor*
+#define вказівник_на_віртуальний_клас abstract*
+#define вказівник_на_новий new*
+#define вказівник_на_видалити delete*
+#define вказівник_на_розмір size*
+#define вказівник_на_вивести_розмір sizeof*
+#define пустий void
+#define вказівник_на_пустий void*
+
+#define дорівнює =
+#define додати +
+#define відняти -
+#define помножити *
+#define поділити /
+#define перевірка_рівності ==
+#define менше <
+#define більше >
+#define менше_або_дорівнює <=
+#define більше_або_дорівнює >=
+#define не_дорівнює !=
+#define і &&
+#define або ||
+#define не !
+#define плюс_дорівнює +=
+#define мінус_дорівнює -=
+#define множина_дорівнює *=
+#define ділення_дорівнює /=
+#define інкремент ++
+#define декремент --
+// define for >> and <<
+#define ввести_з_потоку >>
+#define вивести_у_потік <<
+
+
+#define а a
+#define б b
+#define в c
+#define г d
+#define д e
+#define є f
+#define ж g
+#define з h
+#define и i
+#define і j
+#define й k
+#define к l
+#define л m
+#define м n
+#define н o
+#define о p
+#define п q
+#define р r
+#define с s
+#define т t
+#define у u
+#define ф v
+#include <windows.h>  
+
+#define підключити_українську() { \
+   SetConsoleCP(1251); \
+   SetConsoleOutputCP(1251); \
+}
+#define х w
+#define ц x
+#define ч y
+#define ш z
+#define щ shch
+#define ь myagkij_znak
+#define ю u
+#define я ya
+
+
+#endif
